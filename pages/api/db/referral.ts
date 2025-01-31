a3a866208c1b48439d78951e1140f76182d42b37
Sister Sabrina <sistersabrina100@gmail.com>
2025-01-31 17:32:44 -0300

    made changes to api/db/referral

--- pages/api/db/referral.ts
+++ pages/api/db/referral.ts
@@ -29,7 +29,19 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         res.send("No results.");
       }
     }
-    const referrals = await prisma.referral.findMany();
+    const referrals = await prisma.referral.findMany({
+      select: {
+        amount: true,
+        start: true,
+        end: true,
+        zone: {
+          select: {
+            name: true,
+            zone_id: true,
+          },
+        },
+      },
+    });
     if (referrals.length > 0) {
       res.send(referrals);
     } else {
