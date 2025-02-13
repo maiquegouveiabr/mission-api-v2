5363d011653f5fa61b3a88b1a28a1ca95896d12a
Sister Sabrina <sistersabrina100@gmail.com>
2025-02-12 21:09:42 -0300

    trying to fix filterUniqueEvent

--- util/filterUniqueEvent.ts
+++ util/filterUniqueEvent.ts
@@ -4,8 +4,8 @@ import { ContactAttempt } from "@/interfaces";
 export default (events: ContactAttempt[]) => {
   const eventDays = new Set();
   return events.filter(({ itemDate }) => {
-    const date = new Date(itemDate);
-    const dayKey = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    const date = new Date(itemDate - 3 * 60 * 60 * 1000);
+    const dayKey = date.toISOString().split("T")[0]; // Gets YYYY-MM-DD in UTC
     if (eventDays.has(dayKey)) return false;
     eventDays.add(dayKey);
     return true;
