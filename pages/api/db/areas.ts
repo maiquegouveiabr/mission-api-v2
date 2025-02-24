54d62b2898673afaa7500f20563f3e5819f83dad
Sister Sabrina <sistersabrina100@gmail.com>
2025-02-24 16:21:32 -0300

    fixed: filter is_active for area selector

--- pages/api/db/areas.ts
+++ pages/api/db/areas.ts
@@ -5,6 +5,9 @@ import { prisma } from "@/util/db";
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   try {
     const areas = await prisma.area.findMany({
+      where: {
+        is_active: true,
+      },
       orderBy: {
         name: "asc",
       },
